a6d1e2bd237c5a04b586974b1aa5fde4e56bf74b
Ji Myung Hwa <battlesun99@gmai.com>
2021-07-18 22:09:08 +0900

    Feat: save username using by localStorage

--- momentum/app.js
+++ momentum/app.js
@@ -7,7 +7,11 @@ const HIDDEN_CLASSNAME = "hidden";
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;    
+    const username = loginInput.value;
+
+    // localStorage에 아이템 저장하기
+    localStorage.setItem("username", username);
+
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
