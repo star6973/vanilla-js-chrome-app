5c530d947e4b2f95558ba167f7e9af7c9f50cc88
Ji Myung Hwa <battlesun99@gmai.com>
2021-07-18 23:11:03 +0900

    Feat: create event_handler for savedUserName

--- momentum/app.js
+++ momentum/app.js
@@ -2,18 +2,36 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
+
 const HIDDEN_CLASSNAME = "hidden";
+const USER_NAME = "username";
 
-function onLoginSubmit(event) {
-    event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-
-    // localStorage에 아이템 저장하기
-    localStorage.setItem("username", username);
-
+// localStorage에 있는 username을 가져온 뒤, 텍스트로 표출함
+function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function onLoginSubmit(event) {
+    event.preventDefault();
+    
+    const username = loginInput.value;
+
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    // localStorage에 아이템 저장하기
+    localStorage.setItem(USER_NAME, username);
+
+    paintGreetings(username);
+}
+
+const savedUserName = localStorage.getItem(USER_NAME);
+
+// localStorage에 아이템이 없는 경우, login form 양식을 화면에 표출함
+if (savedUserName === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} 
+// localStorage에 아이템이 있는 경우, login form 양식을 숨기고 username을 화면에 표출함
+else {
+    paintGreetings(savedUserName);
+}
